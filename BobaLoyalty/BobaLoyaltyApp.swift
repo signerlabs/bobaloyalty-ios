4c9731614aad8ffa1c521a0f8bd6f29f77df8c00
John <wei@signerlabs.com>
2026-05-11 14:50:20 +0800

    Initial Commit

---
+++ BobaLoyalty/BobaLoyaltyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobaLoyaltyApp.swift
+//  BobaLoyalty
+//
+//  Created by Wei Zhong on 11/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BobaLoyaltyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
